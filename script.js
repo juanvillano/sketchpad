96f6e01e45dda010c3fa5e2a1a70c8621893f5ad
juanvillano <104040385+juanvillano@users.noreply.github.com>
2026-04-24 13:42:27 -0400

    create function to prompt number of squares

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 
 const container = document.querySelector('#container');
+const containerWidth = container.clientWidth;
+const containerHeight = container.clientHeight;
 
-function createDivs(count) {
+function createDivs(count, width, height) {
     for (let i = 0; i < count; i++) {
-        let div = document.createElement('div');
-        container.appendChild(div);
+        let leDiv = document.createElement('div');
+        leDiv.style.minWidth = `${width}px`;
+        leDiv.style.minHeight = `${height}px`;
+        container.appendChild(leDiv);
     } 
 }
 
-let containerWidth = container.clientWidth;
-let containerHeight = container.clientHeight;
 
-let across = containerHeight / 4;
-let down = containerWidth / 4;
-let total = across * down;
+function getSquaresPerSide() {
 
-console.log('height: ', containerHeight);
-console.log('width: ', containerWidth);
-console.log('total: ', total);
+    const squaresPerSide = parseInt(prompt('How many squares per side? '));
 
-createDivs(total);
+    let across = Math.floor(containerHeight / squaresPerSide);
+    let down = Math.floor(containerWidth / squaresPerSide);
+    let total = squaresPerSide * squaresPerSide;
 
+    console.log('height of square: ', across);
+    console.log('width of square: ', down);
+    console.log('total: ', total);
+
+    createDivs(total, across, down);
+}
+
+
+getSquaresPerSide();
+
+
+// color divs
 const divList = document.querySelectorAll("#container > div");
 divList.forEach((div) => {
     div.addEventListener('mouseenter', (e) => {
